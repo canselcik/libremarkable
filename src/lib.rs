95131b753916c444eb45241024cf6a3a41322118
not-avail <not-avail@not-avail>
2018-04-07 23:45:12 -0700

    Fixing typos in docs

--- src/lib.rs
+++ src/lib.rs
@@ -25,13 +25,13 @@ pub mod framebuffer;
 /// The other core component, allowing decoding of the three input devices present on the tablet
 pub mod input;
 
-/// Simply battery and charging status provider
+/// Simple battery and charging status provider
 pub mod battery;
 
 /// Contains the `ApplicationContext`, which is a general framework that can be used to either build
-/// your application or design your I/O code after. It uses UI rudimentary UI elements and adds them
+/// your application or design your I/O code after. It uses rudimentary UI elements and adds them
 /// to a scene after wrapping them in `UIElementWrapper`. None of these are mandatory to be used.
-/// You can choose to entirely ignore the `ApplicationContext` and `ui_extensions` and choose to
-/// interact with the `framebuffer` and `input` devices directly.
+/// You can choose to entirely ignore the `ApplicationContext` and `ui_extensions` and interact
+/// with the `framebuffer` and `input` devices directly.
 pub mod appctx;
 pub mod ui_extensions;
