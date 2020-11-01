f747f96fb071cf3b5e655dc29b3e34f165a06179
Linus <linus@cosmos-ink.net>
2020-11-01 19:19:17 +0100

    Display the devices in input example

--- examples/input.rs
+++ examples/input.rs
@@ -1,7 +1,18 @@
-use libremarkable::input::{ev::EvDevContext, InputDevice, InputEvent};
+use libremarkable::input::{ev::EvDevContext, ev::INPUT_DEVICE_PATHS, InputDevice, InputEvent};
 use std::sync::mpsc::channel;
 
 fn main() {
+    // Display paths for InputDevices
+    for device in [
+        InputDevice::GPIO,
+        InputDevice::Multitouch,
+        InputDevice::Wacom,
+    ]
+    .iter()
+    {
+        eprintln!("{:?} is {:?}", INPUT_DEVICE_PATHS[device], device);
+    }
+
     // Send all input events to input_rx
     let (input_tx, input_rx) = channel::<InputEvent>();
     EvDevContext::new(InputDevice::GPIO, input_tx.clone()).start();
