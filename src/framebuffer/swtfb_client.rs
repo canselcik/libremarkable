canselcik/libremarkable

commit a02757896761955a1d15789fbe32068f51dc4227

Clarify format of SEM_WAIT_TIMEOUT
Co-authored-by: Pierre Fenoll <pierrefenoll@gmail.com>

src/framebuffer/swtfb_client.rs
@@ -20,7 +20,7 @@ pub const WIDTH: i32 = 1404;
 pub const HEIGHT: i32 = 1872;
 
 pub const BUF_SIZE: i32 = WIDTH * HEIGHT * std::mem::size_of::<u16>() as i32; // hardcoded size of display mem for rM2
-const SEM_WAIT_TIMEOUT: i32 = 200000000; /* 200 * 1000 * 1000, e.g. 200ms */
+const SEM_WAIT_TIMEOUT_NS: i32 = 200_000_000;
 
 /// long on 32 bit is 4 bytes as well!!
 #[derive(Debug, Clone, Copy)]

commit 0c5dbcc7b573ec6cc5cec1f6029ec22fe10b2cb6

Fix error caused by commit a027578 "Clarify format of SEM_WAIT_TIMEOUT"

src/framebuffer/swtfb_client.rs
@@ -166,7 +166,7 @@ impl SwtfbClient {
         unsafe {
             libc::clock_gettime(libc::CLOCK_REALTIME, &mut timeout);
         }
-        timeout.tv_nsec += SEM_WAIT_TIMEOUT;
+        timeout.tv_nsec += SEM_WAIT_TIMEOUT_NS;
 
         if timeout.tv_nsec >= 1_000_000_000 {
             timeout.tv_nsec -= 1_000_000_000;