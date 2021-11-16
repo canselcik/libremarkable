36719d779241527396e1f58d5171fcf506d183f1
Pierre Fenoll <pierrefenoll@gmail.com>
2021-11-17 00:45:07 +0100

    cargo fmt
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- src/framebuffer/swtfb_client.rs
+++ src/framebuffer/swtfb_client.rs
@@ -163,7 +163,8 @@ impl SwtfbClient {
         }
         self.send_wait_update(&wait_sem_data { sem_name });
         let sem_name_c = CString::new(sem_name_str.as_str()).unwrap();
-        let sem = unsafe { libc::sem_open(sem_name_c.as_ptr() as *const libc::c_char, libc::O_CREAT) };
+        let sem =
+            unsafe { libc::sem_open(sem_name_c.as_ptr() as *const libc::c_char, libc::O_CREAT) };
 
         let mut timeout = libc::timespec {
             tv_nsec: 0,
