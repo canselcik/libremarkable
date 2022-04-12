160951979d25d91c236b159921f26e77cc28244c
ju6ge <judge@felixrichter.tech>
2022-04-12 10:01:54 +0200

    added extraction of other build flags to script

--- gen_cargo_config.py
+++ gen_cargo_config.py
@@ -9,9 +9,9 @@ linker = "{gcc_path}"
 rustflags = [
   "-C", "link-arg=-march=armv7-a",
   "-C", "link-arg=-marm",
-  "-C", "link-arg=-mfpu=neon",
-  "-C", "link-arg=-mfloat-abi=hard",
-  "-C", "link-arg=-mcpu=cortex-a9",
+  "-C", "link-arg={fpu}",
+  "-C", "link-arg={float_s}",
+  "-C", "link-arg={cpu}",
   "-C", "link-arg=--sysroot={sysroot}",
 ]\n"""
 
@@ -19,12 +19,20 @@ def main():
     #get sysroot from environment
     sysroot = os.environ["SDKTARGETSYSROOT"]
     #get comiler name from environment
-    cc = os.environ["CC"].split(" ")[0]
+    cc_full = os.environ["CC"]
+    cc = cc_full.split(" ")[0]
     #strip -gcc from compiler to build regex string for parsing PATH
     cc_path = cc[0:-4]
     re_parse_path = ":[^:]*{cc}:".format(cc=cc_path)
     extract_compiler_path = re.compile(re_parse_path)
     toolchain_compiler_path = extract_compiler_path.search(os.environ["PATH"]).group(0)[1:-1]
+    #get other arguments from compiler definition
+    re_fpu = re.compile("(-mfpu=[^ ]*)")
+    re_float = re.compile("(-mfloat-abi=[^ ]*)")
+    re_cpu = re.compile("(-mcpu=[^ ]*)")
+    fpu_arg = re_fpu.search(cc_full).group(0)
+    float_arg = re_float.search(cc_full).group(0)
+    cpu_arg = re_cpu.search(cc_full).group(0)
     #join path to get full compiler path
     cc = os.path.join(toolchain_compiler_path, cc)
     
@@ -32,7 +40,7 @@ def main():
     if not os.path.isdir(".cargo"):
         os.mkdir(".cargo")
     with open(".cargo/config","w") as f:
-        f.write(cargo_config.format(gcc_path=cc, sysroot=sysroot))
+        f.write(cargo_config.format(gcc_path=cc, sysroot=sysroot, fpu=fpu_arg, float_s=float_arg, cpu=cpu_arg))
     print("Toolchain config written to \".cargo/config\" ✅")
 
 if __name__ == "__main__":
